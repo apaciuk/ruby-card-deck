08b4b30debfbfefa348975273d4f57adb38226bc
xhostcom <xhostcom@gmail.com>
2022-01-09 07:42:32 +0000

    Readied for card comparison

--- lib/card.rb
+++ lib/card.rb
@@ -10,15 +10,15 @@ class Card
   end
 
   def to_s
-    "The #{value} of #{@suit}"
+    "The #{human_value} of #{@suit}"
   end
 
   def self.build(suit)
     (1..13).map { |value| new(value, suit) }
   end
 
-  def value
-    case @value
+  def human_value
+    case value
     when 1
       :ace
     when 11
@@ -28,7 +28,12 @@ class Card
     when 13
       :king
     else
-      @value
+      value
     end
   end
+
+  protected
+
+  attr_reader :value
+
 end
